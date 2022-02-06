da52884b8543b937100bed811ba3b88edd5ead16
Rogerio Reis <rsreis@MacBook-Pro-Reis.local>
2022-02-06 11:02:08 -0300

    Ajustando desafio 44

--- desafio044.py
+++ desafio044.py
@@ -1,4 +1,4 @@
-print('########## Desafio 044 ##########')
+print('{:=^40}'.format(' Desafio 044 '))
 
 # recebendo o valor do produto
 valor = float(input('\nInforme o valor do produto: '))
@@ -20,16 +20,20 @@ print(menu)
 forma_pagamento = int(input('\n Escolha uma das formas de pagamento acima: ' ))
 
 if forma_pagamento == 1:
-    valor_final = valor - (valor * 0.10)
+    valor_final = valor - (valor * 10 / 100)
     print('\nO valor final a ser pago com 10% de desconto será R$ {:.2f}'.format(valor_final))
 elif forma_pagamento == 2:
     valor_final = valor - (valor * 0.05)
     print('\nO valor final a ser pago com 5% de desconto será R$ {:.2f}'.format(valor_final))
 elif forma_pagamento == 3:
-    print('\nO valor final a ser pago será o valor original do produto R$ {:.2f}'.format(valor))
+    valor_final = valor
+    parcela = valor_final / 2
+    print('\nO valor final a ser pago será R$ {:.2f} em 2X de R$ {:.2f} SEM JUROS'.format(valor, parcela))
 elif forma_pagamento == 4:
-    valor_final = valor + (valor * 0.20)
-    print('\nO valor final com 20% de juros será R$ {:.2f}'.format(valor_final))
+    valor_final = valor + (valor * 20 / 100)
+    total_parcelas = int(input('\nQuantas parcelas? '))
+    parcela = valor_final / total_parcelas
+    print('\nO valor final com 20% de juros será em {}X de R$ {:.2f} COM JUROS'.format(total_parcelas, parcela))
 else:
-    print('\nVocê deve digitar um dos valores acima citados!')
+    print('\nOpção inválida, você deve digitar um dos valores acima citados!')
 
