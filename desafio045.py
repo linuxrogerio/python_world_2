7ff5aee4cb2e311d60ee3ac3ca88e376fe33d8cd
Rogerio Reis <linux.rogerio@gmail.com>
2022-02-06 12:24:25 -0300

    Subindo ajustes do desafio 45

--- desafio045.py
+++ desafio045.py
@@ -1,12 +1,11 @@
 import random
-import ascii
+from time import sleep
 
-
-print('########## Desafio 045 ##########')
+print('{:=^40}'.format(' Desafio 045 '))
 
 print('\nOlá! Eu quero jogar um jogo com você...')
 
-menu = """ Escolha um dos ítems para jogarmos:
+menu = """\nEscolha um dos ítems para jogarmos:
 
 1 -> Pedra 
 
@@ -16,13 +15,32 @@ menu = """ Escolha um dos ítems para jogarmos:
 
 """
 print(menu)
-escolha = int(input('Escolha umas das opções acima, digitando o número correspondente: '))
+escolha = int(input('\nEscolha umas das opções acima, digitando o número correspondente: '))
 
+#Sorteando a escolha da máquina...
 escolha_maquina = random.choice(range(1, 3))
 
 
-print('\nSua escolha {}'.format(escolha))
-print('\nEscolha da máquina {}'.format(escolha_maquina))
+print('\nJO..')
+sleep(2)
+print('KEN..')
+sleep(2)
+print('PO!!')
 
+print('\nVocê escolheu {}'.format(escolha))
+print('\nA máquina escolheu {}'.format(escolha_maquina))
 
-if 
+if escolha == escolha_maquina:
+    print('Jogo empatado')
+elif escolha == 1 and escolha_maquina == 2:
+    print('papel embrulha pedra - maquina ganha')
+elif escolha == 1 and escolha_maquina == 3:
+    print('Pedra quebra tesoura - Voce ganha')
+elif escolha == 2 and escolha_maquina == 1:
+    print('Papel embrulha pedra - voce ganha')
+elif escolha == 2 and escolha_maquina == 3:
+    print('Tesoura corta papel - maquina ganha')
+elif escolha == 3 and escolha_maquina == 1:
+    print('Tesoura corta papel - voce ganha')
+elif escolha == 3 and escolha_maquina == 2:
+    print('Papel embrulha pedra - maquina ganha')
