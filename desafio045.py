cb183cf6fddfbaae71ba91fd1f54b5201b80ae4a
Rogerio Reis <rsreis@recordtv.com.br>
2022-02-07 22:18:20 -0300

    Modificando a estrutura do desafio 45

--- desafio045.py
+++ desafio045.py
@@ -1,25 +1,22 @@
-import random
+from random import randint
 from time import sleep
 
 print('{:=^40}'.format(' Desafio 045 '))
 
-print('\nOlá! Eu quero jogar um jogo com você...')
+itens = ('Pedra', 'Papel', 'Tesoura')
 
-menu = """\nEscolha um dos ítems para jogarmos:
+menu = """\njogador um dos ítems para jogarmos:
 
-1 -> Pedra 
-
-2 -> Papel
-
-3 -> Tesoura
+0 -> Pedra 
+1 -> Papel
+2 -> Tesoura
 
 """
 print(menu)
-escolha = int(input('\nEscolha umas das opções acima, digitando o número correspondente: '))
+jogador = int(input('\njogador umas das opções acima, digitando o número correspondente: '))
 
-#Sorteando a escolha da máquina...
-escolha_maquina = random.choice(range(1, 3))
-
+#Sorteando a jogador da máquina...
+computador = randint(0, 2)
 
 print('\nJO')
 sleep(1)
@@ -27,21 +24,35 @@ print('KEN')
 sleep(1)
 print('PO!!!')
 
-print(':-=40')
-print('\nVocê escolheu {}'.format(escolha))
-print('\nA máquina escolheu {}'.format(escolha_maquina))
+print('-=' * 11)
+print('\nVocê escolheu {}'.format(itens[jogador]))
+print('\nA máquina escolheu {}'.format(itens[computador]))
+print('=-' * 11)
 
-if escolha == escolha_maquina:
-    print('Jogo empatado')
-elif escolha == 1 and escolha_maquina == 2:
-    print('papel embrulha pedra - maquina ganha')
-elif escolha == 1 and escolha_maquina == 3:
-    print('Pedra quebra tesoura - Voce ganha')
-elif escolha == 2 and escolha_maquina == 1:
-    print('Papel embrulha pedra - voce ganha')
-elif escolha == 2 and escolha_maquina == 3:
-    print('Tesoura corta papel - maquina ganha')
-elif escolha == 3 and escolha_maquina == 1:
-    print('Tesoura corta papel - voce ganha')
-elif escolha == 3 and escolha_maquina == 2:
-    print('Papel embrulha pedra - maquina ganha')
+if computador == 0: #Computador jogou Pedra
+    if jogador == 0:
+        print('EMPATE')
+    elif jogador == 1:
+        print('JOGADOR VENCE')
+    elif jogador == 2:
+        print('COMPUTADOR VENCE')
+    else:
+        print('JOGADA INVÁLIDA!')
+elif computador == 1: #Computador jogou Papel
+    if jogador == 0:
+        print('COMPUTADOR VENCE')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('JOGADOR VENCE')
+    else:
+        print('JOGADA INVÁLIDA!')
+elif computador == 2: #Computador jogou Tesoura
+    if jogador == 0:
+        print('JOGADOR VENCE')
+    elif jogador == 1:
+        print('COMPUTADOR VENCE')
+    elif jogador == 2:
+        print('EMPATE')
+    else:
+        print('JOGADA INVÁLIDA!')
