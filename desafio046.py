72d898c55745d9ff9aafe702cd358a5eaae615de
Rogerio Reis <rsreis@recordtv.com.br>
2022-02-12 14:58:34 -0300

    Subindo desafio 046

---
+++ desafio046.py
@@ -0,0 +1,14 @@
+from time import sleep
+import emoji
+
+
+print('{:=^40}'.format(' Desafio 046 '))
+print('{:#^40}'.format('CONTAGEM REGRESSIVA!! '))
+
+for i in range(10, 0, -1):
+    print('\n',i)
+    sleep(1)
+print('\nFeliz ano novo!!\n')
+
+print(emoji.emojize(':thumbs_up:'))
+
