09bd64e9c2a48a4bae87bd6d967d30d622f58725
Rogerio Reis <rsreis@recordtv.com.br>
2022-02-23 21:36:12 -0300

    Subindo o final do desafio 53

--- desafio053.py
+++ desafio053.py
@@ -1,3 +1,22 @@
+from posixpath import split
+
+
 print('{:=^40}'.format(' Desafio 053 '))
+
 frase = str(input('\nDigite uma frase: ')).strip().upper()
-print('\nVocė digitou a frase {}'.format(frase))
+
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+
+inverso = junto[::-1]
+
+"""for letra in range(len(junto) -1, -1, -1):
+    inverso += junto[letra]"""
+
+print('\nO inverso de {} é {}'.format(junto, inverso))
+
+if inverso == junto:
+    print('\nTemos um Palíndromo!')
+else:
+    print('\nA frase digitada não é um Palíndromo!')
