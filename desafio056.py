11d2ea3e156bdb990baf374800295439723c2b43
Rogerio Reis <linux.rogerio@gmail.com>
2022-03-02 22:44:22 -0300

    Subindo final do desafio 56

--- desafio056.py
+++ desafio056.py
@@ -1,8 +1,10 @@
 print('{:=^40}'.format(' Desafio 056 '))
 
 soma_idade = 0
-velho = 0
-nova = 0
+media_idade = 0
+maior_idade_homem = 0
+nome_mais_velho = ''
+total_mulher_20 = 0
 
 for pessoas in range(1,5):
     print('----- {}º PESSOA -----'.format(pessoas))
@@ -11,8 +13,15 @@ for pessoas in range(1,5):
     sexo = str(input('\nSexo [M/F]: ')).strip()
     sexo = sexo.upper()
     soma_idade += idade
-    
-    
+    if pessoas == 1 and sexo in 'Mm':
+        maior_idade_homem = idade
+        nome_mais_velho = nome
+    if sexo in 'Mm' and idade > maior_idade_homem:
+        maior_idade_homem = idade
+        nome_mais_velho = nome
+    if sexo in 'fF' and idade < 20:
+        total_mulher_20 += 1
 media_idade = soma_idade / 4
-print('A media de idade do grupo é de {} anos.'.format(media_idade))
-  
+print('\nA media de idade do grupo é de {} anos.'.format(media_idade))
+print('\nO homem mais velho tem {} e se chama {}.'.format(maior_idade_homem, nome_mais_velho))
+print('\nAo todo sāo {} mulher com menos de 20 anos.'.format(total_mulher_20))
