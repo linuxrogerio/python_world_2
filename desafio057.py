e010b874a9ddbcb9f4a02935f8c8e9d92831fba9
Rogerio Reis <rsreis@recordtv.com.br>
2022-03-20 10:59:34 -0300

    Subindo desafio 57

---
+++ desafio057.py
@@ -0,0 +1,7 @@
+print('{:=^40}'.format(' Desafio 057 '))
+
+sexo = ' '
+while sexo != "M" or sexo != "F":
+    sexo = str(input('\nDigite o seu sexo: [M/F]: ')).upper()
+    print(sexo)
+print('\nFim do programa.')    
