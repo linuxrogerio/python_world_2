c33eb397e1137159a2d769c58230dfa747417da5
Rogerio Reis <linux.rogerio@gmail.com>
2022-03-20 21:57:04 -0300

    Subindo desafio 57 funcionando

--- desafio057.py
+++ desafio057.py
@@ -1,7 +1,7 @@
 print('{:=^40}'.format(' Desafio 057 '))
 
 sexo = ' '
-while sexo != "M":
+while sexo != "M" and sexo != "F":
     sexo = str(input('\nDigite o seu sexo: [M/F]: ')).upper()
     print(sexo)
 print('\nFim do programa.')
