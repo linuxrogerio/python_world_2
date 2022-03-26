78d56b5c01c1ffc8712a25737c9c2da0fd2c9b8f
Rogerio Reis <linux.rogerio@gmail.com>
2022-03-26 09:49:02 -0300

    Subindo desafio 58 funcionando

--- desafio058.py
+++ desafio058.py
@@ -2,9 +2,18 @@ print('{:=^40}'.format(' Desafio 058 '))
 
 from random import randint
 
-r = randint(0, 5)
+r = randint(0, 10)
+palpite = 1
+chamada = '\nO computador pensou em um número entre 0 e 10 tente adivinha que número é esse: '
+user_num = int(input(chamada))
 
-user_num = int(input('\nO computador pensou em um número tente adivinha que número é esse: '))
+while user_num != r:
+    print('\nVocê ERROU! tente novamente: ')
+    palpite += 1
+    user_num = int(input(chamada))
 
-print(user_num, '--', r)
+print(f'\nVocê ACERTOU! seu palpite foi {user_num} e o numero que a máquina escolheu também foi {r}.')
+print(f'\nVocê tentou {palpite} vezes até acertar!')
+
+
 
