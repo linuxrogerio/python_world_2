4242aaa6b603562161f62637bf446dda393cfa7d
Rogerio Reis <rsreis@recordtv.com.br>
2022-03-29 20:51:25 -0300

    Subindo ajuste no desafio 057

--- desafio057.py
+++ desafio057.py
@@ -1,7 +1,6 @@
 print('{:=^40}'.format(' Desafio 057 '))
 
-sexo = ' '
-while sexo != "M" and sexo != "F":
-    sexo = str(input('\nDigite o seu sexo: [M/F]: ')).upper()
-    print(sexo)
-print('\nFim do programa.')
+sexo = str(input('\nDigite o seu sexo: [M/F]: ')).strip().upper()[0]
+while sexo not in 'mMfF':
+    sexo = str(input('\nDados inválidos, favor digite o seu sexo: [M/F]: ')).strip().upper()[0]
+print(f'\nSexo {sexo} foi registrado com sucesso!.')
