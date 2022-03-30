43d3a9acdc1e15347c7f13d0034c1d9c31353903
Rogerio Reis <rsreis@recordtv.com.br>
2022-03-29 21:13:46 -0300

    Subindo ajustes no desafio 058

--- desafio058.py
+++ desafio058.py
@@ -4,16 +4,20 @@ from random import randint
 
 r = randint(0, 10)
 palpite = 1
-chamada = '\nO computador pensou em um número entre 0 e 10 tente adivinha que número é esse: '
+chamada = """\n
+Olá! Sou seu computador\n
+Acabei de pensar em um número entre 0 e 10.
+\nTente adivinhar que número é esse: """
+
 user_num = int(input(chamada))
 
 while user_num != r:
-    print('\nVocê ERROU! tente novamente: ')
     palpite += 1
-    user_num = int(input(chamada))
+    if user_num > r:
+        user_num = int(input('Menos.. tente novamente: '))
+    else:
+        user_num = int(input('Mais.. tente novamente: '))
+    
 
 print(f'\nVocê ACERTOU! seu palpite foi {user_num} e o numero que a máquina escolheu também foi {r}.')
 print(f'\nVocê tentou {palpite} vezes até acertar!')
-
-
-
