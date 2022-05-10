7b93848cce3589e9ef018abb9cb292566c559776
Rogerio Reis <rsreis@recordtv.com.br>
2022-05-09 21:55:45 -0300

    Subindo desafio 061

--- desafio061.py
+++ desafio061.py
@@ -1,1 +1,13 @@
 print('{:=^40}'.format(' Desafio 061 '))
+
+primeiro = int(input('\nDigite o primeiro termo da PA: '))
+razao = int(input('\nDigite a razão: '))
+termo = primeiro
+cont = 1
+
+while cont <=10:
+    print(f'{termo} -> ', end='')
+    termo += razao
+    cont += 1
+print('FIM\n')
+    
