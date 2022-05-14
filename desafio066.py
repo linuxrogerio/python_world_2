59f47d5df205e8e3eb84ebeeef690b857e051de0
Rogerio Reis <rsreis@recordtv.com.br>
2022-05-14 03:24:50 -0300

    Subindo o desafio 66

---
+++ desafio066.py
@@ -0,0 +1,11 @@
+print('{:=^40}'.format(' Desafio 066 '))
+n = soma = cont = 0
+
+while True:
+    n = int(input('Digite um valor [999 para parar]: '))
+    if n == 999:
+        break
+    soma += n
+    cont += 1
+
+print(f'A soma dos {cont} valores foi {soma}!')
