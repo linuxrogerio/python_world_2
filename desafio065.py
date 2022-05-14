9289c50bf92d420c428b00091dcbb89638a10475
Rogerio Reis <rsreis@recordtv.com.br>
2022-05-14 01:16:03 -0300

    Subindo final do desafio 65

--- desafio065.py
+++ desafio065.py
@@ -1,23 +1,25 @@
 print('{:=^40}'.format(' Desafio 065 '))
 
 msg = 'S'
-cont = total = num = maior = menor = 0
+cont = total = num = media = maior = menor = 0
 
 
 while msg in 'sS':
     num = int(input('Digite um número: '))
-    msg = str(input('Quer continuar? [S/N] ')).lower()
-      
     cont += 1
     total += num
-       
-    if num >= maior:
-        maior = num
-        if num > 0 and menor > maior:
-            menor = num
+      
+    if cont == 1:
+        maior = menor = num
+    else:
+        if num > maior:
+            maior = num
+        if num < menor:
+           menor = num
     
+    
+    msg = str(input('Quer continuar? [S/N] ')).lower().strip()[0]
 media = total / cont
-    
 print('\nAcabou!\n')
 print(f'\nVocê digitou {cont} números e a media deles foi {media:.0f}.\n')
-print(f'\nO maior valor foi {maior} e o menor foi {menor}')
+print(f'\nO maior valor foi {maior} e o menor foi {menor}\n')
