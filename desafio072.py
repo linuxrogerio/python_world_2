1578ca4c9727c4c9b1551a126723719b9bae5c14
Rogerio Reis <rsreis@recordtv.com.br>
2022-05-24 00:05:38 -0300

    Subindo nova versão do desafio 072

--- desafio072.py
+++ desafio072.py
@@ -8,10 +8,8 @@ posicao = 0
 
 while True:
     posicao = int(input("\nDigite um número entre 0 e 20: "))
-    while posicao < 0 or posicao > 20:
-        print('\nTente novamente. ')
-        posicao = int(input("\nDigite um número entre 0 e 20: "))
-    if posicao > 0 or posicao < 20:
+    if 0 < posicao < 20:
         break
+    print('\nTente novamente. ')
         
 print(f'\nVocê digitou o número {valor[posicao]}')
