74c86c9dea3540954e0b89b42b88cdeb14b9538b
Rogerio Reis <rsreis@recordtv.com.br>
2022-05-23 23:01:23 -0300

    subindo aula 16

---
+++ aula16.py
@@ -0,0 +1,31 @@
+#lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim', 'Batata Frita')
+#
+#print(sorted(lanche))
+#print(lanche)
+
+#for comida in lanche:
+#    print(f'Eu vou comer {comida}')
+#
+#for cont in range(0, len(lanche)):
+#    print(f'Eu vou comer {lanche[cont]} na posição {cont}')
+#
+#for pos, comida in enumerate(lanche):
+#    print(f'Eu vou comer {comida} na posição {pos}.')
+#
+#print('Comi pra caramba!')
+
+
+#a = (2, 5, 4)
+#b = (5, 8, 1, 2)
+#c = b + a
+#d = a + b
+#
+#print(len(c)) # tamanho da tupla c
+#print(c)
+#print(d)
+#print(c.count(5)) #quantas vezes aparece o numero 5 em ma tupla "c"
+#print(c.index(8)) # em que posição está o 8
+
+pessoa = ('Reis', 36, "M", 80.90)
+#del(pessoa) #apagando a tupla
+print(pessoa)
