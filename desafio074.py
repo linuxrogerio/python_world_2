06c166fb29e6f4fac6db5cefc08e84201ba6ee3b
Rogerio Reis <rsreis@recordtv.com.br>
2022-06-06 09:40:18 -0300

    Subindo ajustes do desafio 74

--- desafio074.py
+++ desafio074.py
@@ -3,11 +3,9 @@ print('{:=^40}'.format(' Desafio 074 '))
 
 tupla = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
 
-maior = menor = 0
-
 print(f'\nOs valores sorteados foram: ', end=' ')
-for i in range(0, 5):
-    print(tupla[i], end=' ')
+for i in tupla:
+    print(i, end=' ')
         
 print(f'\nO maior número sorteado foi {max(tupla)}.')
-print(f'\nO menor número sorteado foi {min(tupla)}.')
+print(f'O menor número sorteado foi {min(tupla)}.')
