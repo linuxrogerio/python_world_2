57711173f797f66ebbb88ad4d98fcdd3ba66deda
Rogerio Reis <rsreis@recordtv.com.br>
2022-06-08 18:29:43 -0300

    Subindo final do desafio 076

--- desafio076.py
+++ desafio076.py
@@ -1,8 +1,8 @@
 print('{:=^40}'.format(' Desafio 076 '))
 
-print('-'*40)
-print('LISTAGEM DE PREÇOS')
-print('-'*40)
+print('-' * 40)
+print(f'{"LISTAGEM DE PREÇOS":^40}')
+print('-' * 40)
 
 listagem = ('Lapis', 1.75,
             'Borracha', 2,
@@ -13,8 +13,11 @@ listagem = ('Lapis', 1.75,
             'Mochila', 120.32,
             'Canetas', 22.30,
             'Livro', 34.90)
-print(listagem)
+#print(listagem)
 
 for pos in range(0, len(listagem)):
     if pos % 2 == 0:
-        print(f'{listagem[pos]:30}')
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R$ {listagem[pos]:>7.2f} ')
+print('-' * 40)
