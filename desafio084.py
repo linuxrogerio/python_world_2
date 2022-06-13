a23a2a6fd0825f41ebaff6515cd193642172e328
Rogerio Reis <rsreis@recordtv.com.br>
2022-06-13 18:08:14 -0300

    Subindo final do desafio 84

--- desafio084.py
+++ desafio084.py
@@ -3,27 +3,35 @@ print('{:=^40}'.format(' Desafio 084 '))
 pessoa = []
 pessoas = []
 
-maior = menor = cont = 0
+maior = menor = 0
 
 while True:
     pessoa.append(str(input('Digite o nome: ')))
-    pessoa.append(int(input('Digite o peso: ')))
-    pessoas.append(pessoa[:])
-        
-    if cont == 0:
+    pessoa.append(float(input('Digite o peso: ')))
+    if len(pessoas) == 0:
         maior = menor = pessoa[1]
-    elif pessoa[1] > maior:
-        maior = pessoa[1]
-    elif pessoa[1] < menor:
-        menor = pessoa[1]
-        
+    else:
+        if pessoa[1] > maior:
+            maior = pessoa[1]
+        if pessoa[1] < menor:
+            menor = pessoa[1]
+    pessoas.append(pessoa[:])
     pessoa.clear()
-    cont += 1
+           
     opcao = str(input('Quer continuar? [S/N] ')).strip().lower()
     
     if opcao == 'n':
         break
     
-print(pessoas)
+print('-=' * 30)
 print(f'\nAo todo foram cadastradas {len(pessoas)} pessoas!')
-print(f'O maior peso foi {maior}Kg')
+print(f'\nO maior peso foi {maior:.1f}Kg. Peso de ', end='')
+for p in pessoas:
+    if p[1] == maior:
+        print(f'[{p[0]}] ', end='')
+print(f'\nO menor peso foi {menor:.1f}Kg. Peso de ', end='')
+for p in pessoas:
+    if p[1] == menor:
+        print(f'[{p[0]}]', end='')
+print()
+print('-=' * 30)
