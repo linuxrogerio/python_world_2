4cc8495254de44b9d00c162a37caecd38ce3d9db
Rogerio Reis <Rogerio.Reis@gls-group.com>
2022-12-16 18:39:18 +0000

    Simples ajuste no desafio 096

--- desafio096.py
+++ desafio096.py
@@ -2,6 +2,7 @@ def area(l, c):
     tamanho = l * c
     print(f'O tamanho do terreno é: {tamanho}m2')
 
+#Main app
 print('\n---- CONTROLE DE TERRENOS ----\n')
 l = float(input('LARGURA (m): '))
 c = float(input('COMPRIMENTO (m): '))
