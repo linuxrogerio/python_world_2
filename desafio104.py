ef6fe8d46e6f2de12692a6fddb9fef73a6d8f0f0
Rogerio Reis <Rogerio.Reis@gls-group.com>
2023-01-02 10:40:13 +0000

    Subindo o desafio 104

---
+++ desafio104.py
@@ -0,0 +1,20 @@
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+    
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+        if ok:
+            break
+    return valor
+
+
+
+#Main App
+n = leiaInt('Digite um número: ')
+print(f'Você digitou o número {n}.')
